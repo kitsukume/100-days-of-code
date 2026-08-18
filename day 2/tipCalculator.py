1166fdf8be4e32afc2ec70ea551267c6f95df98c
Kitsukume <creamie921@gmail.com>
2026-08-18 18:00:37 -0400

    Update Tip Calculator

--- day 2/tipCalculator.py
+++ day 2/tipCalculator.py
@@ -4,11 +4,6 @@ t_percent = float(input("How much would you like to tip ? %")) / 100 + 1
 t_people = int(input("How many people to split with? "))
 
 
-
-
-
-
-
 t_total = round(t_bill*t_percent/t_people, 2)
 
 print(f"Each person should pay: ${t_total:.2f}")
